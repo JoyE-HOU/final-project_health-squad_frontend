f96fbe96c8dc6bdebeeb418e23356d8885ffd03b
Joy Evans <joyevans@me.com>
2022-05-18 20:33:26 -0500

    configured Auth0Provider

--- health-squad/src/index.js
+++ health-squad/src/index.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,9 +7,19 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  // this code below replaces lines 10-12 when using Aut0
+    <Auth0Provider
+    domain="dev-a7xzy1ja.us.auth0.com"
+    clientId="1gwPF2ejgCe8Ha2iQOfYmDZv9YxWegiT"
+    redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
